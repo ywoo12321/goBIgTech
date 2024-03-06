99176b7e7f56901e888df1cb723b2f2ee3f3a3a5
yeongwoo <ywoo12321@gmail.com>
2024-03-06 10:34:40 +0900

    [FEAT] solve budget

--- algorithm/binarySearch/budget.js
+++ algorithm/binarySearch/budget.js
@@ -9,11 +9,41 @@ const budgetList = input
   .shift()
   .split(" ")
   .map((budget) => Number(budget))
-  .sort();
+  .sort((a, b) => a - b);
 const totalBudget = Number(input.shift());
 
-const parametricSearch = (arr, target, start, end) => {};
+const isBiggerThanTotal = (arr, totalBudget, budget) => {
+  let money = 0;
+  arr.map((budgetMoney) => {
+    if (budgetMoney >= budget) money += budget;
+    else money += budgetMoney;
+  });
+  if (money > totalBudget) return true;
+  else return false;
+};
+
+const binarySearch = (arr, target, start, end) => {
+  const arrSum = arr.reduce((a, b) => a + b);
+  if (arrSum <= target) return end;
+  else {
+    let result = 0;
+    while (start <= end) {
+      const mid = parseInt((start + end) / 2);
+      if (!isBiggerThanTotal(arr, target, mid)) {
+        result = mid;
+        start = mid + 1;
+      } else {
+        end = mid - 1;
+      }
+    }
+
+    return result;
+  }
+};
 
 // 최대 금액 < 상한액 및 요구 금액 합계 break
+// start : localNumber , bugetList[localNumber-1]
 
-console.log(budgetList);
+const answer = binarySearch(budgetList, totalBudget, 1, budgetList[localNumber - 1]);
+
+console.log(answer);

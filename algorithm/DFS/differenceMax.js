8090061dd21995d7bc5770ad308927265dc7925d
yeongwoo <ywoo12321@gmail.com>
2024-03-20 16:09:59 +0900

    [FEAT] solve differenceMax

--- algorithm/DFS/differenceMax.js
+++ algorithm/DFS/differenceMax.js
@@ -6,24 +6,27 @@ const input = require("fs")
 
 const N = Number(input.shift());
 const numberList = input.shift().split(" ").map(Number);
+const visited = new Array(N).fill(false);
+const result = [];
 let maxNumber = -1e9;
 
-const dfs = () => {};
-
-for (let i = 0; i < N; i++) {
-  const copyArray = JSON.parse(JSON.stringify(numberList));
-  const nowNumber = numberList[i];
-  copyArray.splice(i, 1);
-  for (let j = 0; j < N; j++) {
-    copyArray.splice(j, 0, nowNumber);
-    current = 0;
-    for (let k = 0; k < N - 1; k++) {
-      current += Math.abs(copyArray[k] - copyArray[k + 1]);
+const dfs = (count) => {
+  if (count === N) {
+    let current = 0;
+    for (let i = 0; i < N - 1; i++) {
+      current += Math.abs(result[i] - result[i + 1]);
     }
-    console.log(copyArray, current);
-    maxNumber = Math.max(maxNumber, current);
-    copyArray.splice(j, 1);
+    maxNumber = Math.max(current, maxNumber);
   }
-}
+  for (let i = 0; i < N; i++) {
+    if (visited[i]) continue;
+    visited[i] = true;
+    result.push(numberList[i]);
+    dfs(count + 1);
+    visited[i] = false;
+    result.pop();
+  }
+};
+dfs(0);
 console.log(maxNumber);
 //For 문 돌려서 하나씩 뽑아서 모든 위치에 넣고 절대값 계산기 함수 => 최대값 저장
